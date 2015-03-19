c913b619ddc667ac1aa140fb6bb4548a6c70abca
James Conkling <james.lane.conkling@gmail.com>
2015-03-19 15:23:18 -0400

    refactor search into _includes/search.html for use on home and library

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
-var mainbottom = $('.menu').offset().top;
+$(document).ready(function(){
+  var mainbottom = $('.menu').offset().top;
 
-$(window).on('scroll',function(){
+  $(window).on('scroll',function(){
 
-    stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
-        $('.menu').addClass('past-main');
-    } else {
-        $('.menu').removeClass('past-main');
-    }
+      stop = Math.round($(window).scrollTop());
+      if (stop > mainbottom) {
+          $('.menu').addClass('past-main');
+      } else {
+          $('.menu').removeClass('past-main');
+      }
+  });
+
+  $("#hideshow").click(function(){
+    $("#advanced").slideToggle(500);
+  });
+
 });
